4639be7b36346f54f7b5a2eafa118e3cc7c51f89
Luis Fernando Domingo Gomez <al222010066@gmail.com>
2022-05-15 15:20:01 -0500

    Searching and Filtering

--- nestjs-task-magement/src/tasks/tasks.service.ts
+++ nestjs-task-magement/src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,31 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((tasks) => {
+        // eslint-disable-next-line prettier/prettier
+        if (tasks.title.includes(search) || tasks.description.includes(search)) {
+          return true;
+        }
+
+        return false;
+      });
+    }
+
+    return tasks;
   }
 
   getTaskById(id: string): Task {
