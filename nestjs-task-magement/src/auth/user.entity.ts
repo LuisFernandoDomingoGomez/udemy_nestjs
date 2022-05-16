a6729da4b8ca603c604cffe9113d03288fb7addf
Luis Fernando Domingo Gomez <al222010066@gmail.com>
2022-05-16 14:28:43 -0500

    Setting up AuthModule, User Entity and User Repository

---
+++ nestjs-task-magement/src/auth/user.entity.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+}
