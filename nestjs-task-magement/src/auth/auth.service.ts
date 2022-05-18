61557fbd19b1ec4c646cb4cabfd953614906ced0
Luis Fernando Domingo Gomez <al222010066@gmail.com>
2022-05-18 10:38:55 -0500

    Signing Up

--- nestjs-task-magement/src/auth/auth.service.ts
+++ nestjs-task-magement/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
@@ -9,4 +10,8 @@ export class AuthService {
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 }
