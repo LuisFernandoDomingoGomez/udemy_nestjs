3db0afae4f1a695d6bacf2c12675aa8285894c41
Luis Fernando Domingo Gomez <al222010066@gmail.com>
2022-05-23 01:01:23 -0500

    Front-end Application

--- nestjs-task-magement/src/tasks/dto/update-task-status.dto.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { IsEnum } from 'class-validator';
-import { TaskStatus } from '../task-status.enum';
-
-export class UpdateTaskStatusDto {
-  @IsEnum(TaskStatus)
-  status: TaskStatus;
-}
